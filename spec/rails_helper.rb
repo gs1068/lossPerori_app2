747a802150a0ed4435127e05b7feabebf1f126bd
gs1068 <genta.otias@gmail.com>
2021-01-31 09:11:02 +0900

    RSpecとrubocop環境構築

---
+++ spec/rails_helper.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+require "capybara/rspec"
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
+  config.use_transactional_fixtures = true
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+end
