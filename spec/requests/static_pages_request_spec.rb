930d3e67d07604ebdaac7c38db6a882f17da1a7a
gs1068 <genta.otias@gmail.com>
2021-02-02 16:17:59 +0900

    devise導入とUserモデル作成2

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe "StaticPages", type: :request do
   describe "rootパスからホームページにアクセスできること" do
     it "returns http success" do
       get root_path
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 end
