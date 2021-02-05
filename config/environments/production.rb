653eb1334f3d3d6f7ec141d375b340f11115a6a4
gs1068 <genta.otias@gmail.com>
2021-02-05 11:45:30 +0900

    herokuのGmail環境設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = {  :host => 'https://dry-dusk-76134.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'smtp.gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:DEV_SMTP_USER_NAME],
+    password: Rails.application.credentials.gmail[:DEV_SMTP_PASSWORD],
+    authentication: :login
+  }
 end
