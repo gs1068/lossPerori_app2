bdc5070ed4022ca0acf2bf0995970dcebfd778de
gs1068 <genta.otias@gmail.com>
2021-02-05 14:56:06 +0900

    user modelスペックを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
-# FactoryBot.define do
-#   factory :user do
-#   end
-# end
+FactoryBot.define do
+  factory :user do
+    username { "TestUser" }
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
