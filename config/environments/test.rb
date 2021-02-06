e2a7fee0cd5fe8715e3b01a6ce099e2cf8adfcab
gs1068 <genta.otias@gmail.com>
2021-02-06 11:18:52 +0900

    CircleCIへmaster.keyの追加2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,11 +61,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'smtp.gmail.com',
-    port:587,
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
     user_name: Rails.application.credentials.gmail[:DEV_SMTP_USER_NAME],
     password: Rails.application.credentials.gmail[:DEV_SMTP_PASSWORD],
-    authentication: :login
   }
 end
