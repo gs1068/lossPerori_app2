97f0bc40339ecf45d4b27f5859f7f531efacf235
gs1068 <genta.otias@gmail.com>
2021-02-18 10:13:32 +0900

    product systemスペック実装完了

--- spec/system/product_setting.rb
+++ spec/system/product_setting.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'ProductNew', type: :system do
+RSpec.describe 'Products', type: :system do
   before do
     ActionMailer::Base.deliveries.clear
   end
@@ -51,10 +51,50 @@ RSpec.describe 'ProductNew', type: :system do
     expect(page).to have_content 'ありがとうございます。商品の出品が完了しました。'
 
     # 商品の編集
-    # user = User.first
-    # product = user.products.first
-    # visit edit_product_path(product.id)
+    user = User.first
+    product = user.products.first
 
-    # binding.pry
+    visit new_user_session_path
+    fill_in 'login-email', with: user.email
+    fill_in 'login-password', with: 'foobar'
+    click_button 'ログイン'
+    expect(page).to have_content 'ログインしました。'
+
+    # 自分の商品の場合は編集ページが存在する
+    visit product_path(product.id)
+    expect(page).to have_content '編集する'
+    click_link '編集する'
+
+    # 商品の編集
+    # 無効な編集
+    expect(page).to have_content '商品の編集'
+    fill_in '商品名', with: ''
+    fill_in '商品紹介', with: ''
+    fill_in '原材料', with: ''
+    fill_in '価格', with: ''
+    fill_in '消費期限', with: ''
+    fill_in '総重量', with: ''
+    click_button '更新する'
+    expect(page).to have_content '商品の編集'
+    expect(page).to have_content 'エラーが8個あります。'
+
+    visit product_path(product.id)
+    click_link '編集する'
+
+    # 有効な編集
+    attach_file 'product[product_avatars][]', 'spec/system/test3.jpg'
+    fill_in '商品名', with: 'Test-Product2'
+    fill_in '商品紹介', with: 'hogehogefoobar2'
+    fill_in '原材料', with: 'foobar2'
+    fill_in '価格', with: '4321'
+    fill_in '消費期限', with: '002024-12-31'
+    fill_in '総重量', with: '2.5'
+    click_button '更新する'
+    expect(page).to have_content '商品の編集が完了しました。'
+
+    # 商品の削除
+    visit product_path(product.id)
+    click_link '編集する'
+    expect { click_link '商品を削除する' }.to change { Product.all.size }.by(-1)
   end
 end
